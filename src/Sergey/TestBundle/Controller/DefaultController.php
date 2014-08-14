9ea968b5563be64a04b444b80e9ef43930b35aba
Sergey Moiseev <sergey.moiseev@funcfunc.ru>
2014-08-14 09:35:57 +0400

    #time 37m - Profile form and editing

--- src/Sergey/TestBundle/Controller/DefaultController.php
+++ src/Sergey/TestBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Sergey\TestBundle\Controller;
 
+use Sergey\TestBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -24,5 +25,44 @@ class DefaultController extends Controller
         ];
     }
 
+    /**
+     * @Route("/profile", name="profile")
+     * @Template()
+     */
+    public function profileAction(Request $request)
+    {
+        $session = $this->container->get('session');
+        if (is_null($user = $this->getUser())) {
+            $session->getFlashBag()->add('error', 'You need to be authorized for access to edit profile');
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+        $form = $this->createForm(
+            new UserType(),
+            $user,
+            []
+        );
+
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Profile updated');
+
+            return $this->redirect($this->generateUrl('profile'));
+        }
+        elseif($form->isSubmitted())
+        {
+            $this->get('session')->getFlashBag()->add('error', 'Error while submitting form');
+        }
+
+        return [
+            'form' => $form->createView(),
+        ];
+    }
 
 }
